b997b6b99cbd0ebf893f941522fc4fea1bb05d99
AstroSnail <astrosnail@protonmail.com>
2025-06-20 12:56:32 +0000

    Change pname of patched xterm

--- xterm.nix
+++ xterm.nix
@@ -5,6 +5,7 @@
       # directly seems to cause a large cascade of rebuilds
       xterm-erry =
         prev.xterm.overrideAttrs (oldattrs: {
+          pname = "xterm-erry";
           # remove sixel-256.support.patch
           patches = [
             ./xterm-f1-f4.patch
