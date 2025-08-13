e15e8a876f0b1ef0f27338a3c96652b979f750d1
AstroSnail <astrosnail@protonmail.com>
2025-08-13 20:19:11 +0000

    Fix escaping in vim-drop

--- vim-drop.sh
+++ vim-drop.sh
@@ -3,6 +3,6 @@ case ${filename} in
 	(/*) filepath=${filename};;
 	(*) filepath=${PWD}/${filename};;
 esac
-# TODO: escape special characters
+filejson=$(printf %s "${filepath}" | jq --raw-input --slurp)
 # shellcheck disable=SC1003
-printf '\e]51;["drop", "%s"]\e\\' "${filepath}"
+printf '\e]51;["drop", %s]\e\\' "${filejson}"
