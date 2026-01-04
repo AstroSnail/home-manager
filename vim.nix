53679831a52feecc72b7df6bee2d4eec65fa47be
AstroSnail <astrosnail@protonmail.com>
2026-01-04 01:39:08 +0000

    Patch :terminal's TERM handling

--- vim.nix
+++ vim.nix
@@ -7,6 +7,7 @@
         prev.vim-full.overrideAttrs (oldattrs: {
           patches = oldattrs.patches ++ [
             ./vim-termguicolors.patch
+            ./vim-terminal-env.patch
           ];
         });
     })
