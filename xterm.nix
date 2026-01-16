869de2c4768f3b727bec28a909d6f85cc8068409
AstroSnail <astrosnail@protonmail.com>
2026-01-16 10:11:26 +0000

    Update xterm.nix

--- xterm.nix
+++ xterm.nix
@@ -14,17 +14,15 @@
           configureFlags = oldattrs.configureFlags ++ [
             "--enable-block-select"
             # "--enable-status-line"
-            "--enable-toolbar"
-            #"--enable-trace"
+            # "--enable-toolbar"
+            # "--enable-trace"
           ];
         });
     })
   ];
 
   xresources.properties = {
-    # some of these are redundant against UXTerm-color
-    # but they're so benign i might as well just config for plain XTerm
-
+    # Feature settings
     # custom TERM assumes xterm has been configured in specific ways
     # e.g. patches above and keyboardType below
     # "XTerm.termName" = "xterm-erry";
@@ -33,56 +31,63 @@
     # "XTerm*VT100.decGraphicsID" = 340;
     # "XTerm*VT100.numColorRegisters" = 1024;
 
-    #"XTerm.toolBar" = true;
+    # Widget settings
+    # "XTerm.toolBar" = true;
     "XTerm*menubar.borderWidth" = 1;
     "XTerm*VT100.borderWidth" = 0;
-    #"XTerm*VT100.background" = "black";
-    #"XTerm*VT100.foreground" = "gray90";
-    #"XTerm*VT100.boldColors" = false;
-    #"XTerm*VT100.cursorBar" = true;
-    #"XTerm*VT100.cursorBlink" = true;
-    #"XTerm*VT100.cursorUnderLine" = true;
-    #"XTerm*VT100.showWrapMarks" = true;
+    # TODO: disable scrollbar only in alternate screen
+    # "XTerm*VT100.scrollBar" = true;
+    # "XTerm*VT100.rightScrollBar" = true;
+
+    # Text settings
+    # "XTerm*VT100.background" = "black";
+    # "XTerm*VT100.foreground" = "gray90";
+    # "XTerm*VT100.boldColors" = false;
+    # "XTerm*VT100.cursorBar" = true;
+    # "XTerm*VT100.cursorBlink" = true;
+    # "XTerm*VT100.cursorUnderLine" = true;
+    # "XTerm*VT100.showWrapMarks" = true;
     "XTerm*VT100.colorBD" = "white"; # default bold makes bright white
     "XTerm*VT100.colorBDMode" = true;
     "XTerm*VT100.veryBoldColors" = 4;
-
-    #"XTerm*VT100.faceName" =
-    #  "x:-misc-fixed-medium-r-normal--18-120-100-100-c-90-iso10646-1";
-    #"XTerm*VT100.faceNameDoublesize" =
-    #  "x:-misc-fixed-medium-r-normal-ja-18-120-100-100-c-180-iso10646-1";
-    #"XTerm*VT100.faceName" = "Unifont";
-    #"XTerm*VT100.faceSize" = "12";
+    # "XTerm*VT100.faceName" =
+    #   "x:-misc-fixed-medium-r-normal--18-120-100-100-c-90-iso10646-1";
+    # "XTerm*VT100.faceNameDoublesize" =
+    #   "x:-misc-fixed-medium-r-normal-ja-18-120-100-100-c-180-iso10646-1";
+    # "XTerm*VT100.faceName" = "Unifont";
+    # "XTerm*VT100.faceSize" = "12";
     "XTerm*VT100.faceName" = "Dina";
     "XTerm*VT100.faceSize" = "10";
 
+    # Bell settings
     "XTerm*VT100.bellIsUrgent" = true;
     "XTerm*VT100.visualBell" = true;
     "XTerm*VT100.visualBellLine" = true;
 
-    # TODO: disable scrollbar only in alternate screen
-    #"XTerm*VT100.scrollBar" = true;
-    "XTerm*VT100.rightScrollBar" = true;
+    # Scrolling settings
     "XTerm*VT100.scrollKey" = true;
     "XTerm*VT100.scrollTtyOutput" = false;
-    #"XTerm*VT100.allowScrollLock" = true;
+    # "XTerm*VT100.allowScrollLock" = true;
     "XTerm*VT100.autoScrollLock" = true;
     "XTerm*VT100.cdXtraScroll" = true;
-    #"XTerm.buffered" = true; # holy shit this causes SO MANY GLITCHES
-    "XTerm*VT100.jumpScroll" = true;
-    "XTerm*VT100.fastScroll" = true;
-    "XTerm*VT100.multiScroll" = true;
+    # "XTerm.buffered" = true; # holy shit this causes SO MANY GLITCHES
+    # "XTerm*VT100.jumpScroll" = true;
+    # "XTerm*VT100.fastScroll" = true;
+    # "XTerm*VT100.multiScroll" = true;
 
+    # Input settings
     # "XTerm.keyboardType" = "vt220";
-    #"XTerm.ttyModes" = "erase ^h";
+    # "XTerm.ttyModes" = "erase ^h";
     # ncurses ships terminfo with xterm+kbs set to DEL when it's built
     # on a linux system, but xterm default-enables backarrowKey at
     # build time (ie backarrow=BS).
     "XTerm*VT100.backarrowKey" = false;
-    "XTerm*VT100.eightBitInput" = false;
+    # "XTerm*VT100.eightBitInput" = false;
     "XTerm*VT100.locale" = true;
-    #"XTerm*VT100.modifyOtherKeys" = 2;
+    "XTerm*VT100.metaSendsEscape" = true;
+    # "XTerm*VT100.modifyOtherKeys" = 2;
 
+    # Translation settings
     "XTerm.omitTranslation" = [
       # "shift-fonts" # gets in the way of shift+kp_add in vt220 keyboard mode
     ];
@@ -90,7 +95,7 @@
     # flawed: this only works correctly when the window is focused and the
     #         pointer is over the window.
     # workaround: release and re-press shift before releasing the mouse
-    #             button, listen out for MinorError bells.
+    #             button, listen/watch out for MinorError bells.
     "XTerm*VT100.translations" = ''
       #override \n\
       <KeyPress> Shift_L: set-select(on) \n\
