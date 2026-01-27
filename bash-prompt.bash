AstroSnail/home-manager

commit e1a2db48bfd05967391054188137742831d7b58d

Eliminate prompt expansion from prompt-extra
and other small refactors

bash-prompt.bash
@@ -1,17 +1,19 @@
 # shellcheck disable=SC2016
 
 erry_tput() {
-	local IFS=$'\n'
+	local IFS
+	IFS=$'\n'
 	tput -S <<<"$*"
 }
 
 erry_show_pipestatus() {
-	local p
+	local IFS p this_status
+
 	for p in "${!erry_pipestatus[@]}"
 	do
 		if [[ ${erry_pipestatus[p]} -ne 0 ]]
 		then
-			local this_status=
+			this_status=
 			this_status+=$(erry_tput 'bold' 'setaf 1')
 			this_status+=${erry_pipestatus[p]}
 			this_status+=$(erry_tput 'sgr0')
@@ -19,7 +21,7 @@ erry_show_pipestatus() {
 		fi
 	done
 
-	local IFS=\|
+	IFS=\|
 	printf %s "${erry_pipestatus[*]}"
 }
 
@@ -46,17 +48,20 @@ erry_gen_prompt_extra() {
 	output+=$(erry_tput 'bold' 'setaf 2')
 	output+=PIPESTATUS
 	output+=$(erry_tput 'sgr0')
-	output+='=($(erry_show_pipestatus))\n'
+	output+='=(@PIPESTATUS@)'
+	output+=$'\n'
 
 	output+=$(erry_tput 'bold' 'setaf 2')
 	output+=PWD
 	output+=$(erry_tput 'sgr0')
-	output+='=\w\n'
+	output+='=@PWD@'
+	output+=$'\n'
 
 	output+=$(erry_tput 'bold' 'setaf 2')
 	output+=SHLVL
 	output+=$(erry_tput 'sgr0')
-	output+='=${SHLVL}\n'
+	output+='=@SHLVL@'
+	output+=$'\n'
 
 	# preserve prior newline
 	# strip this dot after command substitution
@@ -66,14 +71,23 @@ erry_gen_prompt_extra() {
 }
 
 erry_show_prompt_extra() {
-	# save pipestatus so it's visible through
-	# command substitution (subshell)
-	local erry_pipestatus=("${PIPESTATUS[@]}")
+	local erry_pipestatus output replace
+	erry_pipestatus=("${PIPESTATUS[@]}")
+	output=${erry_prompt_extra}
 
-	local cufs=$((COLUMNS > 4 ? COLUMNS - 4 : 1))
-	local erry_prompt_extra_ready=${erry_prompt_extra/'%p1%d'/${cufs}}
+	replace=$((COLUMNS > 4 ? COLUMNS - 4 : 1))
+	output=${output/'%p1%d'/${replace}}
 
-	printf %s "${erry_prompt_extra_ready@P}"
+	replace=$(erry_show_pipestatus)
+	output=${output/@PIPESTATUS@/${replace}}
+
+	replace='\w'
+	output=${output/@PWD@/${replace@P}}
+
+	replace=${SHLVL}
+	output=${output/@SHLVL@/${replace}}
+
+	printf %s "${output}"
 
 	# we don't need to restore $? as long as the command
 	# gets its own place in the $PROMPT_COMMAND array

bash-title.bash
@@ -9,13 +9,12 @@ erry_set_title() {
 }
 
 erry_visual_escape() {
-	local output=${1}
+	local output oct c0 pc0
+	output=${1}
 
 	# replace all C0 controls with their printable forms
-	local oct
 	for oct in {0..3}{0..7}
 	do
-		local c0 pc0
 		printf -v c0 %b '\00'"${oct}"
 		printf -v pc0 %b '\01'"${oct}"
 		output=${output//${c0}/^${pc0}}
@@ -28,12 +27,14 @@ erry_visual_escape() {
 
 erry_show_prompt_title() {
 	# bash already sanitizes the expansion of \w
-	local title='\w'
+	local title
+	title='\w'
 	erry_set_title "${title@P}"
 }
 
 erry_show_command_title() {
-	local title=$(erry_visual_escape "${BASH_COMMAND}")
+	local title
+	title=$(erry_visual_escape "${BASH_COMMAND}")
 	erry_set_title "${title}"
 }
 
@@ -44,6 +45,7 @@ then
 	# PS0 doesn't get an up-to-date BASH_COMMAND :<
 	# DEBUG trap will have to do
 	# TODO: append to trap
+	# TODO: does this check actually even work?
 	if [[ -n $(trap -p DEBUG) ]]
 	then printf 'DEBUG trap conflict!\n' >&2
 	fi

commit 343c0cdd5ff2a87c453b0f99bc543cb329b51ecf

Fix pipestatus in prompt
bug introduced in e1a2db48bfd05967391054188137742831d7b58d

bash-prompt.bash
@@ -72,8 +72,10 @@ erry_gen_prompt_extra() {
 }
 
 erry_show_prompt_extra() {
-	local erry_pipestatus output replace
-	erry_pipestatus=("${PIPESTATUS[@]}")
+	# save pipestatus first
+	local erry_pipestatus=("${PIPESTATUS[@]}")
+
+	local output replace
 	output=${erry_prompt_extra}
 
 	replace=$((COLUMNS > 2 ? COLUMNS - 2 : 1))