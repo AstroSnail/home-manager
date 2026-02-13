c83a14529ac3d248478f103a7212ec354a3cb9d9
AstroSnail <astrosnail@protonmail.com>
2026-02-13 11:08:01 +0000

    Remove quotes on associative-array keys
    turns out the variable expansion magic only works on indexed arrays

--- bash-prompt.bash
+++ bash-prompt.bash
@@ -1,30 +1,30 @@
 erry_gen_tput() {
 	declare -Ag erry_tput
 
-	erry_tput['cr']=$(tput cr)
+	erry_tput[cr]=$(tput cr)
 
 	# parameter left unset, placeholder will be replaced later
-	erry_tput['cuf']=$(tput cuf)
+	erry_tput[cuf]=$(tput cuf)
 
 	# ideally SGR 1;31
-	erry_tput['fR']=$(tput bold)
-	erry_tput['fR']+=$(tput setaf 1)
+	erry_tput[fR]=$(tput bold)
+	erry_tput[fR]+=$(tput setaf 1)
 
 	# ideally SGR 1;32
-	erry_tput['fG']=$(tput bold)
-	erry_tput['fG']+=$(tput setaf 2)
+	erry_tput[fG]=$(tput bold)
+	erry_tput[fG]+=$(tput setaf 2)
 
 	# ideally SGR 22;39, or SGR 0
 	# might benefit from adding op?
-	erry_tput['fo']=$(tput sgr0)
+	erry_tput[fo]=$(tput sgr0)
 
 	# TODO: test non-xenl terminal
 	if ! tput am || [[ ${TERM} == dumb ]]; then
-		erry_tput['wrap']=$'\n'
+		erry_tput[wrap]=$'\n'
 	elif tput xenl; then
-		erry_tput['wrap']='  '
+		erry_tput[wrap]='  '
 	else
-		erry_tput['wrap']=' '
+		erry_tput[wrap]=' '
 	fi
 
 	# OSC 2 doesn't seem to have a *specific* corresponding terminfo code.
@@ -36,25 +36,25 @@ erry_gen_tput() {
 	# In case they don't, el may be used after tsl if eslok is set,
 	# otherwise dsl should be used *before* tsl.
 	if tput hs; then
-		erry_tput['title']=
+		erry_tput[title]=
 		if ! tput eslok; then
-			erry_tput['title']+=$(tput dsl)
+			erry_tput[title]+=$(tput dsl)
 		fi
-		if ! erry_tput['title']+=$(tput TS); then
-			erry_tput['title']+=$(tput tsl 0)
+		if ! erry_tput[title]+=$(tput TS); then
+			erry_tput[title]+=$(tput tsl 0)
 		fi
 		if tput eslok; then
-			erry_tput['title']+=$(tput el)
+			erry_tput[title]+=$(tput el)
 		fi
 		# placeholder will be replaced later
-		erry_tput['title']+=%s
-		erry_tput['title']+=$(tput fsl)
+		erry_tput[title]+=%s
+		erry_tput[title]+=$(tput fsl)
 	elif [[ ${TERM} == xterm* ]]; then
 		# Annoyingly, xterm terminfo descriptions don't include a
 		# status/title line.
-		erry_tput['title']=$'\e]2;%s\e\\'
+		erry_tput[title]=$'\e]2;%s\e\\'
 	else
-		erry_tput['title']=
+		erry_tput[title]=
 	fi
 }
 
@@ -62,10 +62,10 @@ erry_fix_eol() {
 	local cuf n
 	n=$((COLUMNS > 2 ? COLUMNS - 2 : 1))
 
-	if [[ ${erry_tput['wrap']} == $'\n' ]]; then
+	if [[ ${erry_tput[wrap]} == $'\n' ]]; then
 		cuf=
-	elif [[ -n ${erry_tput['cuf']} ]]; then
-		cuf=${erry_tput['cuf']/'%p1%d'/${n}}
+	elif [[ -n ${erry_tput[cuf]} ]]; then
+		cuf=${erry_tput[cuf]/'%p1%d'/${n}}
 	else
 		# no cuf? no problem! just spam spaces
 		# could use cuf1 if available (non-destructive space)
@@ -73,7 +73,7 @@ erry_fix_eol() {
 		printf -v cuf '%*s' "${n}" ''
 	fi
 
-	printf %s "${cuf}${erry_tput['wrap']}${erry_tput['cr']}"
+	printf %s "${cuf}${erry_tput[wrap]}${erry_tput[cr]}"
 }
 
 erry_show_info() {
@@ -84,17 +84,17 @@ erry_show_info() {
 	local p
 	for p in "${!pipestatus[@]}"; do
 		if [[ ${pipestatus[p]} -ne 0 ]]; then
-			pipestatus[p]=${erry_tput['fR']}${pipestatus[p]}${erry_tput['fo']}
+			pipestatus[p]=${erry_tput[fR]}${pipestatus[p]}${erry_tput[fo]}
 		fi
 	done
 	IFS=\|
-	output+=("${erry_tput['fG']}PIPESTATUS${erry_tput['fo']}=(${pipestatus[*]})")
+	output+=("${erry_tput[fG]}PIPESTATUS${erry_tput[fo]}=(${pipestatus[*]})")
 
 	local pwd
 	pwd='\w'
-	output+=("${erry_tput['fG']}PWD${erry_tput['fo']}=${pwd@P}")
+	output+=("${erry_tput[fG]}PWD${erry_tput[fo]}=${pwd@P}")
 
-	output+=("${erry_tput['fG']}SHLVL${erry_tput['fo']}=${SHLVL}")
+	output+=("${erry_tput[fG]}SHLVL${erry_tput[fo]}=${SHLVL}")
 
 	IFS=' '
 	printf '%s\n' "${output[*]}"
@@ -104,7 +104,7 @@ erry_set_title_prompt() {
 	# bash already sanitizes the expansion of \w
 	local title
 	title='\w'
-	printf %s "${erry_tput['title']/'%s'/${title@P}}"
+	printf %s "${erry_tput[title]/'%s'/${title@P}}"
 }
 
 erry_set_title_command() {
@@ -121,7 +121,7 @@ erry_set_title_command() {
 	# and DEL
 	title=${title//$'\177'/^?}
 
-	printf %s "${erry_tput['title']/'%s'/${title}}"
+	printf %s "${erry_tput[title]/'%s'/${title}}"
 }
 
 # save on tput calls
