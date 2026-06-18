b3542804e526570a0989205bf061e16b9b7a1b8d
AstroSnail <astrosnail@protonmail.com>
2026-06-18 10:19:57 +0000

    Remove caps:escape option from wayvnc
    tigervnc doesn't suppress the caps lock led, and the esc key is
    reachable enough on the old laptop anyway

--- packages.nix
+++ packages.nix
@@ -214,7 +214,8 @@ in {
   services.wayvnc.settings.address = "::1";
   services.wayvnc.settings.port = 5900;
   services.wayvnc.settings.xkb_layout = "us";
-  services.wayvnc.settings.xkb_options = "caps:escape,compose:menu";
+  # services.wayvnc.settings.xkb_options = "caps:escape,compose:menu";
+  services.wayvnc.settings.xkb_options = "compose:menu";
   services.wayvnc.settings.xkb_variant = "altgr-weur";
   systemd.user.services.protonmail-bridge.Install.WantedBy = lib.mkForce [ ];
 
