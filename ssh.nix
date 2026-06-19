6a6691af59158af93cfbcc0826c15c391e1a23a1
AstroSnail <astrosnail@protonmail.com>
2026-06-19 14:38:37 +0000

    Replace %n with %h in ControlPath

--- ssh.nix
+++ ssh.nix
@@ -14,7 +14,7 @@
     HashKnownHosts = false;
     UserKnownHostsFile = "~/.ssh/known_hosts";
     ControlMaster = "auto";
-    ControlPath = "~/.ssh/master-%r@%n:%p";
+    ControlPath = "~/.ssh/master-%r@[%h]:%p";
     ControlPersist = "no";
     ConnectTimeout = 10;
   };
