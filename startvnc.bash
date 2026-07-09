1f66cd24f45fc6f4748db6682c1aeb2386cb8d33
AstroSnail <astrosnail@protonmail.com>
2026-07-09 19:07:39 +0000

    Add gpu option, add TODO

--- startvnc.bash
+++ startvnc.bash
@@ -5,6 +5,7 @@
 
 # FIXME: sleep and guessing WAYLAND_DISPLAY is bad :(
 # startx had a similar problem, how was it solved?
+# TODO: try waiting on sway session target
 WLR_BACKENDS=headless WLR_LIBINPUT_NO_DEVICES=1 \
 </dev/null >|~/.local/share/sway-headless-o.txt 2>|~/.local/share/sway-headless-e.txt \
 sway &
@@ -13,7 +14,7 @@ sleep 1
 
 WAYLAND_DISPLAY=wayland-1 \
 </dev/null >|~/.local/share/wayvnc-o.txt 2>|~/.local/share/wayvnc-e.txt \
-wayvnc --max-fps=12 &
+wayvnc --gpu --max-fps=12 &
 
 erry_pulse_tunnel=$(pactl load-module module-tunnel-sink server=::1)
 erry_pulse_exit() {
