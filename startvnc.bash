5245261a923bd8e16a9235a25eab11b21b17d7dd
AstroSnail <astrosnail@protonmail.com>
2026-08-13 09:50:08 +0000

    Get vnc max fps from args

--- startvnc.bash
+++ startvnc.bash
@@ -3,6 +3,8 @@
 #   pactl load-module module-native-protocol-tcp listen=::1 auth-anonymous=true
 #   ssh -L '[::1]:5900:[::1]:5900' -R '[::1]:4713:[::1]:4713'
 
+fps=${1:-12}
+
 # FIXME: sleep and guessing WAYLAND_DISPLAY is bad :(
 # startx had a similar problem, how was it solved?
 # TODO: try waiting on sway session target
@@ -14,7 +16,7 @@ sleep 1
 
 WAYLAND_DISPLAY=wayland-1 \
 </dev/null >|~/.local/share/wayvnc-o.txt 2>|~/.local/share/wayvnc-e.txt \
-wayvnc --gpu --max-fps=12 &
+wayvnc --gpu --max-fps="${fps}" &
 
 erry_pulse_tunnel=$(pactl load-module module-tunnel-sink server=::1)
 erry_pulse_exit() {
